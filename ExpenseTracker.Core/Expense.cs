c4da9b538a9f0289addaf523988667e8249691a3
Ульяна Злобина <ulya2001.uz@gmail.com>
2026-08-28 17:19:14 +0500

    Добавлен DELETE и Id для расходов (CRUD: create, read, update, delete)

--- ExpenseTracker.Core/Expense.cs
+++ ExpenseTracker.Core/Expense.cs
@@ -5,6 +5,7 @@ using System.Text;
 namespace ExpenseTracker.Core
 {    public class Expense
     {
+        public int Id { get; set; }
         public decimal Amount { get; set; }
         public string Category { get; set; } = string.Empty;
         public DateTime Date { get; set; }
