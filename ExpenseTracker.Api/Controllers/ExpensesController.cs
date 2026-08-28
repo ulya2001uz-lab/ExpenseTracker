020793cfb062a99c80f9f01cb870abf686991264
Ульяна Злобина <ulya2001.uz@gmail.com>
2026-08-28 17:19:14 +0500

    Добввлен DELETE и Id для расходов (CRUD: create, read, update, delete)

--- ExpenseTracker.Api/Controllers/ExpensesController.cs
+++ ExpenseTracker.Api/Controllers/ExpensesController.cs
@@ -8,6 +8,7 @@ namespace ExpenseTracker.Api.Controllers
     public class ExpensesController : ControllerBase      //ControllerBase - дает готовые возможности отвечать на запросы
     {
         private static List<Expense> expenses = new();
+        private static int nextId = 1;
 
         [HttpGet]                                       //метка над методом GetAll(): "этот метод отвечает на GET-запрос" (отдать данные)
         public List<Expense> GetAll()                   //возвращает список расходов
@@ -17,9 +18,17 @@ namespace ExpenseTracker.Api.Controllers
         [HttpPost]                                      //метка "этот метод отвечает на POST-запрос" (принять данные), в отличие от [HttpsGet] 
         public Expense Add(Expense expense)             //метод добавления расхода
         {
+            expense.Id = nextId;
+            nextId++;
             expense.Date = DateTime.Now;
             expenses.Add(expense);
             return expense;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)                      //метод удаления одного расхода по его ID
+        {
+            expenses.RemoveAll(x => x.Id == id);
         }
     }
 }
