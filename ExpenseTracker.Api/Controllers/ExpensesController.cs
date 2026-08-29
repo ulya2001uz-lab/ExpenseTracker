7fb4a5b529135990526045d8c5f014ace86a68f0
Ульяна Злобина <ulya2001.uz@gmail.com>
2026-08-30 03:35:43 +0500

    Контроллер переведен на работу с базой - расходы сохраняются в SQL Server, также есть возмодность редактировать или удалять расходы

--- ExpenseTracker.Api/Controllers/ExpensesController.cs
+++ ExpenseTracker.Api/Controllers/ExpensesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ExpenseTracker.Core;
+using ExpenseTracker.Api.Data;
 
 namespace ExpenseTracker.Api.Controllers
 {
@@ -7,41 +8,50 @@ namespace ExpenseTracker.Api.Controllers
     [Route("expenses")]         //задает адрес, контроллер отвечает по пути/expenses
     public class ExpensesController : ControllerBase      //ControllerBase - дает готовые возможности отвечать на запросы
     {
-        private static List<Expense> expenses = new();
-        private static int nextId = 1;
+        private readonly AppDbContext _context;     //поле, где контроллер будет хранить доступ к базе
+
+        public ExpensesController(AppDbContext dbContext)
+        {
+            _context = dbContext;
+        }                                               //сохраняем полученную базу в поле, чтобы пользоваться ей во всех методах
 
         [HttpGet]                                       //метка над методом GetAll(): "этот метод отвечает на GET-запрос" (отдать данные)
         public List<Expense> GetAll()                   //возвращает список расходов
         {
-            return expenses;
+            return _context.Expenses.ToList();
         }
         [HttpPost]                                      //метка "этот метод отвечает на POST-запрос" (принять данные), в отличие от [HttpsGet] 
         public Expense Add(Expense expense)             //метод добавления расхода
         {
-            expense.Id = nextId;
-            nextId++;
             expense.Date = DateTime.Now;
-            expenses.Add(expense);
+            _context.Expenses.Add(expense);
+            _context.SaveChanges();
             return expense;
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)                      //метод удаления одного расхода по его ID
         {
-            expenses.RemoveAll(x => x.Id == id);
+            Expense foundExpense = _context.Expenses.Find(id);
+            if (foundExpense != null)
+            {
+                _context.Remove(foundExpense);
+                _context.SaveChanges();
+            }
         }
 
         [HttpPut("{id}")]
-        public Expense Update([FromRoute] int id,[FromBody] Expense updated)          //метод для изменения расхода
-        {                                                                             //FromRoute/FromBody - указывают asp.net откуда брать параметры
-            Expense found = expenses.FirstOrDefault(expense => expense.Id == id);
-            if (found != null)
+        public Expense Update([FromRoute] int id, [FromBody] Expense updated)          //метод для изменения расхода
+        {                                                                             //FromRoute/FromBody - указывают asp.net откуда брать параметры            
+            Expense foundExpense = _context.Expenses.Find(id);
+            if (foundExpense != null)
             {
-                found.Amount = updated.Amount;
-                found.Category = updated.Category;
-                found.Date = DateTime.Now;
+                foundExpense.Amount = updated.Amount;
+                foundExpense.Category = updated.Category;
+                foundExpense.Date = DateTime.Now;
+                _context.SaveChanges();
             }
-            return found;
+            return foundExpense;
         }
     }
 }
