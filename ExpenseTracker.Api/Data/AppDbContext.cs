b8bb77921af3a06dee8f97a5aaca308541c56bf9
Ульяна Злобина <ulya2001.uz@gmail.com>
2026-08-29 23:51:06 +0500

    Подключен EF Core и SQL Server, создана база и таблица Expenses через миграцию

---
+++ ExpenseTracker.Api/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ExpenseTracker.Core;
+
+namespace ExpenseTracker.Api.Data
+{
+    public class AppDbContext:DbContext //базовый класс EF Core дает всю функциональность работы с БД
+    {
+        //конструктор ниже - это "настройки подключения", которые EF передаст при запуске (адрес базы и тд)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Expense> Expenses { get; set; }        //объявляем таблицу Expenses; DbSet<Expense> - это как список, но в базе
+    }
+}
--- ExpenseTracker.Api/Program.cs
+++ ExpenseTracker.Api/Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using ExpenseTracker.Api.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+//регистрируем AppDbContext.cs как сервис приложения
+builder.Services.AddDbContext<AppDbContext>     
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection"))); //база работает через SQL Server + строка подключения
 
 var app = builder.Build();
 
